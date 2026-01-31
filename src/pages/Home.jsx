2b09e3e68f40b59b513b0807fee14b89552d253d
Shillo Nft <shillo.leaks@gmail.com>
2026-01-31 15:58:46 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { base44 } from '@/api/base44Client';
 import { useQuery } from '@tanstack/react-query';
+import { motion } from 'framer-motion';
 
 // ===== Coin Pop Animation =====
 function CoinPopLayer({ pops }) {
